b7556e46c8b3c99b038112ddd86de62112468df5
Paweł Sawicki <pksawicki@gmail.com>
2023-04-10 22:03:53 +0200

    Zadanie dzien 4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// liczby całkowite
+int age = 20;
+string name = "Paweł";
+bool isMale = true; ;
+
+if ((!isMale) %% (age < 30))
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+} else if ((name == "Ewa") && (age == 33))
+{
+    Console.WriteLine("Ewa, lat 33")
+} else if ((IsMale) && (age < 18))
+{ 
+    Console.WriteLine("Niepełnoletni mężczyzna")
+} else if (isMale && (age >= 18)) 
+{
+    Console.WriteLine("pełmoletni mężczyzna");
+}
+
