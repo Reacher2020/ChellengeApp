eedb9afcf3a4705ca33d9d40560abf0c1315790b
Paweł Sawicki <pksawicki@gmail.com>
2023-05-07 15:22:55 +0200

    Dzien 8 Type tests

---
+++ ChellengeApp.Tests/TypeTests.cs
@@ -0,0 +1,60 @@
+﻿namespace ChellengeApp.Tests
+{
+    public class TypeTests
+    {
+
+        [Test]
+        public void CheckTwoFloatIsEqaul()
+        {
+            // Arrange
+            float number1 = 134.45E-2f;
+            float number2 = 134.45E-2f;
+
+            // Act
+
+            // Assert
+            Assert.AreEqual(number1, number2);
+        }
+
+        [Test]
+        public void CheckTwoIntegerIsEqual()
+        {
+            // Arrange
+            int Age1 = 39;
+            int Age2 = 39;
+            // Act
+
+            // Assert
+            Assert.AreEqual(Age1, Age2);
+        }
+
+        [Test]
+        public void GetUserNameShouldReturnTheSameValue()
+        {
+            // Arrange
+            var employee1 = GetEmployee("Adam", "Nowak", 39);
+            var employee2 = GetEmployee("Adam", "Nowak", 39);
+            // Act
+
+            // Assert
+            Assert.AreEqual(employee1.Imie, employee2.Imie);
+        }
+
+        [Test]
+        public void GetUserSohuldReturnDifferentObject()
+        {
+            // Arrange
+            var employee1 = GetEmployee("Adam", "Nowak", 39);
+            var employee2 = GetEmployee("Adam", "Nowak", 39);
+            // Act
+
+            // Assert
+            Assert.AreNotEqual(employee1, employee2);
+        }
+
+        private Employee GetEmployee(string name, string surname, int age)
+        {
+            return new Employee(name, surname, age);
+        }
+    }
+}
---
+++ ChellengeApp.Tests/UnitTest1.cs
@@ -0,0 +1,38 @@
+namespace ChellengeApp.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void TestAddingScoreForEmployee()
+        {
+            // Arrange
+            var employee = new Employee("Natalia", "Nowak", 28);
+            employee.AddScore(5);
+            employee.AddScore(9);
+            employee.AddScore(-10);
+            employee.AddScore(6);
+
+            // Act
+            var result = employee.TotallScore;
+
+            // Assert
+            Assert.AreEqual(10, result);
+        }
+
+        [Test]
+        public void TestSetEmployeeAge()
+        {
+            // Arrange
+            var employee = new Employee("Natalia", "Nowak", 28);
+            employee.SetAge(28);
+            employee.SetAge(31);
+            employee.SetAge(29);
+
+            // Act
+            var result = employee.Wiek;
+
+            // Assert
+            Assert.AreEqual(29, result);
+        }
+    }
+}
