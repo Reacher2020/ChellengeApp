76300a31217478ca1cdf83aa9cdf4e2772415bb3
Paweł Sawicki <pksawicki@gmail.com>
2023-05-13 01:25:07 +0200

    Dzien 9

---
+++ Employee.cs
@@ -0,0 +1,39 @@
+﻿namespace ChellengeApp
+{
+    public class Employee
+    {
+        public List<float> Grades = new List<float>();
+        public Employee(string name, string surname)
+        {
+            this.Name = name; 
+            this.Surname = surname;
+        }
+
+        public string Name { get; private set;  }
+        public string Surname { get; private set; }
+
+        public void AddGrade(float grade)
+        {
+           this.Grades.Add(grade);
+        }
+
+        public Statistics GetStatistics()
+        {
+            var statistics = new Statistics(); ;
+
+            statistics.Average = 0;
+            statistics.Min = float.MaxValue;
+            statistics.Max = float.MinValue;
+
+            foreach (var grade in this.Grades)
+            {
+                statistics.Min = Math.Min(statistics.Min, grade);
+                statistics.Max = Math.Max(statistics.Max, grade);
+                statistics.Average += grade; 
+            }
+            statistics.Average /= this.Grades.Count;
+
+            return statistics;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,65 +1,10 @@
-﻿Employee employee1 = new Employee("Adam", "kowalski",30);
-Employee employee2 = new Employee("Ewa", "Nowak", 28);
-Employee employee3 = new Employee("Karol", "Mickiewicz", 46);
-Employee employee4 = new Employee("Natalia", "Gorska", 57);
+﻿using ChellengeApp;
 
-Random rnd = new Random();
-
-List<Employee> employees = new List<Employee>()
-{
-    employee1, employee2, employee3, employee4
-};
-
-int MaxTotallScore = -1;
-
-Employee employeeWithMaxScore = null;
-
-for (int i=0;i < 5; i++)
-{
-    foreach (var employee in employees)
-    {
-        employee.AddScore(rnd.Next(1, 11));
-    }
-}
-
-foreach(var employee in employees)
-{
-    if (employee.TotallScore > MaxTotallScore)
-    {
-        employeeWithMaxScore = employee;
-    }
-}
-
-Console.WriteLine(employeeWithMaxScore.Imie + "  " + employeeWithMaxScore.Nazwisko + " Wiek " + employeeWithMaxScore.Wiek + " TotallScore " + employeeWithMaxScore.TotallScore);
-
-public class Employee
-{
-    public string Imie { get; private set; }
-    public string Nazwisko { get; private set; }
-    public int Wiek { get; private set; }
-
-    private List<int> score = new List<int>();
-
-    public Employee(string imie, string nazwisko, int wiek)
-    {
-        this.Imie = imie;
-        this.Nazwisko = nazwisko;
-        this.Wiek = wiek;
-    }
-    public void SetAge (int wiek)
-    {
-        this.Wiek = wiek;
-    }
-
-    public void AddScore (int score)
-    {
-        this.score.Add(score);
-    }
-
-    public int TotallScore { 
-        get
-        {
-            return this.score.Sum();
-        }
-    }
-}
+var employee = new Employee("Paweł", "Sawicki");
+employee.AddGrade(1);
+employee.AddGrade(2);
+employee.AddGrade(7);
+var statistics = employee.GetStatistics();
+Console.WriteLine($"Max: {statistics.Max}");
+Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Min: {statistics.Min}");
